9638d0e1e51d63bc6e3a8654d223e9c240b03368
Rajesh Thokala <rajeshthokala10@gmail.com>
2022-06-23 19:04:43 +0530

    testing changes

---
+++ codebase/Java/HashMapTest.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class HashMapTest{
+public static void main(String args[]){
+HashMap map=new HashMap();
+map.put(null,"rajesh");
+map.put(null,"test");
+System.out.println(map.get(null));
+}
+}
